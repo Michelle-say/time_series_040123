c998d0d427e65b128ec7c2b26a6ed8d2e43fbbd3
michelle <michelle.saypraseut@corrosia.com>
2023-01-05 11:09:39 +0100

    analyse avec bdd18092

--- src/request.py
+++ src/request.py
@@ -6,11 +6,9 @@ def get_object_with_date(session, first, last):
     return(session.query(sncf).filter(
             sncf.FULL_DATE>=first, sncf.FULL_DATE<last).all())
 
-def moyenne_par_mois(session):
-    return(session.query(sncf.__getattribute__).all())
 
 if __name__=="__main__":
-    engine = create_engine("sqlite:///./data/sncf.db")
+    engine = create_engine("sqlite:///./sncf.db")
     Session = sessionmaker(bind=engine)
     with Session() as session:
-        print(moyenne_par_mois(session))
+        print(get_object_with_date(session, "2016-01-01", "2017-01-01"))
